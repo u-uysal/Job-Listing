1f2407ece81cc60dbfc7950bc9fe5391ec930187
ufuk <ufuk.uysl5@gmail.com>
2020-08-07 16:19:40 +0200

    Add languages and tools section

--- src/components/JobPresent.js
+++ src/components/JobPresent.js
@@ -1,30 +1,35 @@
 import React from "react";
 
 export default function JobPresent({ job }) {
+  const arrRoleandLevel = [job.role, job.level];
+  const arraLangandTool = [...job.languages, ...job.tools];
   return (
-    <div className="flex bg-white shadow-lg m-4 p-6">
+    <div className="flex bg-white shadow-lg m-4 p-6 rounded">
       <div>
         <img src={job.logo} alt={job.company} />
       </div>
       <div className="flex flex-col justify-between ml-4">
-        <h3 className="font-bold  text-teal-500">{job.company}</h3>
+        <h3 className="font-bold  text-teal-500">
+          {job.company}
+          {job.new && <span>New</span>}
+        </h3>
         <h2 className="font-bold text-lg">{job.position}</h2>
         <p className="text-gray-700">
           {job.postedAt} - {job.contract} - {job.location}
         </p>
       </div>
       <div className="flex items-center ml-auto">
-        {job.languages
-          ? job.languages.map((lang, i) => (
+        {arrRoleandLevel
+          ? arrRoleandLevel.map((ral, i) => (
               <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
-                {lang}
+                {ral}
               </span>
             ))
           : ""}
-        {job.tools
-          ? job.tools.map((tool, i) => (
+        {arraLangandTool
+          ? arraLangandTool.map((lat, i) => (
               <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
-                {tool}
+                {lat}
               </span>
             ))
           : ""}
