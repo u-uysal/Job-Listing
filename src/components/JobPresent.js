a9f41f154b38441c632e193ab790161d4dc74de7
ufuk <ufuk.uysl5@gmail.com>
2020-08-07 20:51:08 +0200

    Add moble version style

--- src/components/JobPresent.js
+++ src/components/JobPresent.js
@@ -4,9 +4,13 @@ export default function JobPresent({ job }) {
   const arrRoleandLevel = [job.role, job.level];
   const arraLangandTool = [...job.languages, ...job.tools];
   return (
-    <div className="flex bg-white shadow-lg m-4 p-6 rounded">
+    <div className="flex flex-col bg-white shadow-lg my-16 mx-10 p-6 rounded">
       <div>
-        <img src={job.logo} alt={job.company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20"
+          src={job.logo}
+          alt={job.company}
+        />
       </div>
       <div className="flex flex-col justify-between ml-4">
         <h3 className="font-bold  text-teal-500">
@@ -22,22 +26,22 @@ export default function JobPresent({ job }) {
             </span>
           )}
         </h3>
-        <h2 className="font-bold text-lg">{job.position}</h2>
+        <h2 className="font-bold text-lg my-2">{job.position}</h2>
         <p className="text-gray-700">
           {job.postedAt} - {job.contract} - {job.location}
         </p>
       </div>
-      <div className="flex items-center ml-auto">
+      <div className="flex flex-wrap items-center mt-4 mx-4 pt-4 border-t-2 border-gray-500 border-solid">
         {arrRoleandLevel
           ? arrRoleandLevel.map((ral, i) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
                 {ral}
               </span>
             ))
           : ""}
         {arraLangandTool
           ? arraLangandTool.map((lat, i) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
                 {lat}
               </span>
             ))
