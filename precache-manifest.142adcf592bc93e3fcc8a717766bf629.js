fbe704b51c03e63e63c13a5cc0e0b492698050bf
ufuk <ufuk.uysl5@gmail.com>
2020-08-07 23:18:16 +0200

    Updates

---
+++ precache-manifest.142adcf592bc93e3fcc8a717766bf629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a70ec6f75d25bf4d3a45cf1308890d2",
+    "url": "/Job-Listing/index.html"
+  },
+  {
+    "revision": "affbdb0191fe90dc3790",
+    "url": "/Job-Listing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6a6c442f2020e7ff8a78",
+    "url": "/Job-Listing/static/js/2.855649a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Job-Listing/static/js/2.855649a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affbdb0191fe90dc3790",
+    "url": "/Job-Listing/static/js/main.29328a62.chunk.js"
+  },
+  {
+    "revision": "9c81036bef4f94490540",
+    "url": "/Job-Listing/static/js/runtime-main.ec62454a.js"
+  }
+]);
